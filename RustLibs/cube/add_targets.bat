dc5221997eef0e7a8c48bff68ca5ef77a6c939ee
Eric Olson <eric.rob.olson@gmail.com>
2020-05-16 10:01:45 -0700

    Started wiring up build chain for all targets.

---
+++ RustLibs/cube/add_targets.bat
@@ -0,0 +1,6 @@
+rustup target add i686-pc-windows-msvc
+rustup target add x86_64-pc-windows-msvc
+rustup target add armv7-apple-ios
+rustup target add aarch64-linux-android armv7-linux-androideabi i686-linux-android
+
+pause
